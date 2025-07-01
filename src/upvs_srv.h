1bd81a062cb8eb8311730e82e6893712e972c539
proglyk <ilyapronyashin23@yandex.ru>
2025-07-01 11:40:17 +0300

    Added: upvs_srv__get

--- src/upvs_srv.h
+++ src/upvs_srv.h
@@ -17,6 +17,7 @@ void  upvs_srv__del(upvs_srv_t *);
 // new
 param_t *upvs_srv__get_prm(upvs_srv_t *self, u32_t idx);
 int upvs_srv__set( upvs_srv_t *, const u8_t *, u32_t, const u8_t * );
+s32_t	upvs_srv__get( upvs_srv_t *, u32_t, u8_t *, u8_t *, u32_t );
 
 // setter
 s32_t    upvs_srv__set_prm_b(upvs_srv_t *, u32_t, bool, bool);
