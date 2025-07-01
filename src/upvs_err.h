89609cceb5cf114a680ec1ba669b93e59a0d1d31
proglyk <ilyapronyashin23@yandex.ru>
2025-07-01 11:46:21 +0300

    Added structs: 'upvs_err_buf_t', 'upvs_errdesc_t', funcs: upvs_err__init(..), upvs_err__del(..) etc; Modified: 'upvs_err_t'

--- src/upvs_err.h
+++ src/upvs_err.h
@@ -20,15 +20,68 @@ typedef struct {
 #else
 #error "Must be defined CLT or SRV"
 #endif
-} err_cell_t;
+} err_item_t;
+
+typedef struct {
+  struct {
+    // аварийные данные
+    int code;
+    int priority;
+    u8_t* pcDescribe;
+    u8_t* pcInfluence;
+    u8_t* pcHelp;
+  } err;
+  struct {
+    // Буферы динамич. текста
+    u8_t acDescr[128];
+    u8_t acHelp[512];
+  } dyn_buf;
+} upvs_err_buf_t;
+
+typedef s32_t (*dyntext_fn_t)(u8_t *, u32_t, const u8_t *);
+
+typedef struct {
+  // Код
+  int code;
+  // Приоритет
+  int priority;
+  // Неисправность
+  //char* pcName;
+  // Краткое описание для машиниста
+  u8_t* pcDescribe;
+  // Влияние на работу
+  u8_t* pcInfluence;
+  // Помощь для ПЭМа
+  u8_t* pcHelp;
+  // доп.функционал
+  dyntext_fn_t pvDynDesc;
+  dyntext_fn_t pvDynHelp;
+} upvs_errdesc_t;
 
 typedef struct {
   // аварийные записи
-  err_cell_t  axCells[ERR_CAPACITY];
+  err_item_t     axItems[ITEMS_LENGHT];
   // указатель на аварию в контексте текущего обмена по modbus tcp
-  err_cell_t *pxCell;
+  u32_t          ulIdx;
+  upvs_err_buf_t xErrBuf;
+  upvs_errdesc_t const *paxDesc;
 } upvs_err_t;
 
-void *upvs_err__init();
+
+
+void *upvs_err__create();
+int   upvs_err__init(upvs_err_t *);
+void  upvs_err__del(upvs_err_t *);
+s32_t upvs_err__get_free_pos(upvs_err_t *);
+// public
+#if   (defined CLT)
+s32_t upvs_err__check_state(upvs_err_t *, u32_t, bool, s32_t);
+#elif (defined SRV)
+err_item_t *upvs_err__next(upvs_err_t *);
+bool  upvs_err__is_help_equal(upvs_err_t *, s32_t , const u8_t *);
+s32_t upvs_err__get_buf(upvs_err_t *, u32_t, u32_t);
+s32_t upvs_err__get_item_idx(upvs_err_t *, s32_t);
+err_item_t *upvs_err__get_item(upvs_err_t *, u32_t);
+#endif
 
 #endif //_UPVS_ERR_H_
