f1c6300562d1f7dca31fc2de2a56a0d14c226ae0
proglyk <ilyapronyashin23@yandex.ru>
2025-07-02 22:10:51 +0300

    Defines for 'clt_conn' and 'mqtt' has added

--- src/upvs_conf.h
+++ src/upvs_conf.h
@@ -5,12 +5,13 @@
 
 #define UPVS_PARAM_VALUE_SIZE           (64UL)
 #define UPVS_TOPICPATH_SIZE             (64UL)
+#define UPVS_CLT_ERROR_SIZE             2048U
 
 // Размер буферов. для upvs_srv2.h
 #define DECODER_BUFSIZE_RX              (2048UL)
 #define DECODER_BUFSIZE_TX              ( 256UL)
 
-#define ITEMS_LENGHT                    (10UL)
+#define UPVS_ERR_LIST_LENGHT            (10UL)
 
 // Размеры промежуточного буфера аварий. для upvs_err.h
 // размеры массивов соответствуют таковым в таблицах 2..7 Протокола ПК-ПИ396
@@ -19,5 +20,10 @@
 #define HELP_LENGHT  (1024UL) // или 512 (4x128) симв. UTF-8
 
 #define MQTT_BUFSIZE                    2048
+
+#define UPVS_SOCK_RCVTIME               10000U
+
+#define UPVS_CLT_CNT_INIT               (7UL)//(7UL)
+#define UPVS_CLT_CNT_MAX                (69UL)//(57UL)//(70UL)//(23UL)//(62UL)
 
 #endif //_UPVS_CONF_H_
