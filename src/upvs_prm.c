2e63dad3b49b1ea13b24a5504a607e6030a7e3d0
proglyk <ilyapronyashin23@yandex.ru>
2025-07-04 08:34:07 +0300

    Constants 'pcType...', funcs 'upvs_prm__dec_type','upvs_prm__set_type' added, func 'upvs_prm__init' fixed

--- src/upvs_prm.c
+++ src/upvs_prm.c
@@ -13,6 +13,24 @@ static s32_t set_f(value_t *, float, bool);
 static s32_t set_all(value_t *, bool, bool);
 static s32_t set_str(value_t *, const u8_t *, bool);
 
+// Константы
+
+//static const char* pcText = "АО";
+static const char* pcTypeState1 = "ready";
+static const char* pcTypeState2 = "not_ready";
+static const char* pcTypeState3 = "work";
+static const char* pcTypeState4 = "alarm";
+static const char* pcTypeState5 = "APV";
+static const char* pcTypeState6 = "off";
+static const char* pcTypePower1 = "grid380v";
+static const char* pcTypePower2 = "diesel";
+static const char* pcTypePower3 = "line3kv";
+static const char* pcTypePower4 = "ab110v";
+static const char* pcTypeAb1 = "90KGL375P 60A";
+static const char* pcTypeAb2 = "56PzV385P 60A";
+static const char* pcTypeAb3 = "56PzV385P 40A";
+static const char* pcTypeAb4 = "No battery";
+
 // Функции, доступные извне
 
 /**	----------------------------------------------------------------------------
@@ -51,6 +69,18 @@ int
   // memcpy( (void *)self->axList,         // dest в области .heap
           // (const void *)axOriginList,   // src в области .data
           // sizeof(axOriginList) );       // size = n*sizeof(struct)
+  // vsp
+  upvs_prm__set_type(self->paxList[6].xValue.mag.ac, 1); // state
+  // nsp
+  upvs_prm__set_type(self->paxList[16].xValue.mag.ac, 1); // state
+  // ch1
+  upvs_prm__set_type(self->paxList[28].xValue.mag.ac, 1); // state
+  // ch2
+  upvs_prm__set_type(self->paxList[37].xValue.mag.ac, 1); // state
+  // ch3
+  upvs_prm__set_type(self->paxList[45].xValue.mag.ac, 1); // state
+  // ch4
+  upvs_prm__set_type(self->paxList[52].xValue.mag.ac, 1); // state
   
   return 0;
 }
@@ -209,22 +239,80 @@ u32_t
 /*----------------------------------------------------------------------------*/
   u32_t code = 0;
 
-  if      (!strcmp((const char *)pstr, "ready")) code = 1;
-  else if (!strcmp((const char *)pstr, "not_ready")) code = 2;
-  else if (!strcmp((const char *)pstr, "work")) code = 3;
-  else if (!strcmp((const char *)pstr, "alarm")) code = 4;
-  else if (!strcmp((const char *)pstr, "APV")) code = 5;
-  else if (!strcmp((const char *)pstr, "off")) code = 6;
-  else if (!strcmp((const char *)pstr, "grid380v")) code = 7;
-  else if (!strcmp((const char *)pstr, "diesel")) code = 8;
-  else if (!strcmp((const char *)pstr, "line3kv")) code = 9;
-  else if (!strcmp((const char *)pstr, "ab110v")) code = 10;
-  else if (!strcmp((const char *)pstr, "90KGL375P 60A")) code = 11;
-  else if (!strcmp((const char *)pstr, "56PzV385P 60A")) code = 12;
-  else if (!strcmp((const char *)pstr, "56PzV385P 40A")) code = 13;
-  else if (!strcmp((const char *)pstr, "No battery")) code = 14;
+  if      (!strcmp((const char *)pstr, pcTypeState1)) code = 1;
+  else if (!strcmp((const char *)pstr, pcTypeState2)) code = 2;
+  else if (!strcmp((const char *)pstr, pcTypeState3)) code = 3;
+  else if (!strcmp((const char *)pstr, pcTypeState4)) code = 4;
+  else if (!strcmp((const char *)pstr, pcTypeState5)) code = 5;
+  else if (!strcmp((const char *)pstr, pcTypeState6)) code = 6;
+  else if (!strcmp((const char *)pstr, pcTypePower1)) code = 7;
+  else if (!strcmp((const char *)pstr, pcTypePower2)) code = 8;
+  else if (!strcmp((const char *)pstr, pcTypePower3)) code = 9;
+  else if (!strcmp((const char *)pstr, pcTypePower4)) code = 10;
+  else if (!strcmp((const char *)pstr, pcTypeAb1)) code = 11;
+  else if (!strcmp((const char *)pstr, pcTypeAb2)) code = 12;
+  else if (!strcmp((const char *)pstr, pcTypeAb3)) code = 13;
+  else if (!strcmp((const char *)pstr, pcTypeAb4)) code = 14;
   
   return code;
+}
+
+/**	----------------------------------------------------------------------------
+	* @brief Возвращает строковое представление типа
+	* @param code: Код типа в числовом представлении
+	* @retval Указатель на строку */
+const char *
+  upvs_prm__dec_type(u32_t code) {
+/*----------------------------------------------------------------------------*/
+  const char *ptr = NULL;
+  
+  if      (code ==  1) ptr = pcTypeState1;
+  else if (code ==  2) ptr = pcTypeState2;
+  else if (code ==  3) ptr = pcTypeState3;
+  else if (code ==  4) ptr = pcTypeState4;
+  else if (code ==  5) ptr = pcTypeState5;
+  else if (code ==  6) ptr = pcTypeState6;
+  else if (code ==  7) ptr = pcTypePower1;
+  else if (code ==  8) ptr = pcTypePower2;
+  else if (code ==  9) ptr = pcTypePower3;
+  else if (code == 10) ptr = pcTypePower4;
+  else if (code == 11) ptr = pcTypeAb1;
+  else if (code == 12) ptr = pcTypeAb2;
+  else if (code == 13) ptr = pcTypeAb3;
+  else if (code == 14) ptr = pcTypeAb4;
+  
+  return ptr;
+}
+
+/**	----------------------------------------------------------------------------
+	* @brief ???
+  * @param pDest: ???
+	* @param code: ???
+	* @retval ??? */
+s32_t
+  upvs_prm__set_type(u8_t *pDest, u32_t code) {
+/*----------------------------------------------------------------------------*/  
+  if (!pDest) return -1;
+  
+  switch (code) {
+    case  1: strcpy((char *)pDest, pcTypeState1); break;
+    case  2: strcpy((char *)pDest, pcTypeState2); break;
+    case  3: strcpy((char *)pDest, pcTypeState3); break;
+    case  4: strcpy((char *)pDest, pcTypeState4); break;
+    case  5: strcpy((char *)pDest, pcTypeState5); break;
+    case  6: strcpy((char *)pDest, pcTypeState6); break;
+    case  7: strcpy((char *)pDest, pcTypePower1); break;
+    case  8: strcpy((char *)pDest, pcTypePower2); break;
+    case  9: strcpy((char *)pDest, pcTypePower3); break;
+    case 10: strcpy((char *)pDest, pcTypePower4); break;
+    case 11: strcpy((char *)pDest, pcTypeAb1); break;
+    case 12: strcpy((char *)pDest, pcTypeAb2); break;
+    case 13: strcpy((char *)pDest, pcTypeAb3); break;
+    case 14: strcpy((char *)pDest, pcTypeAb4); break;
+    default: return -1;
+  }
+  
+  return 0;
 }
 
 // Функции, ограниченные областью видимости данного файла
