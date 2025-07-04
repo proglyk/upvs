8dec4785f6e907ae991f424692fca06d8c8c1130
proglyk <ilyapronyashin23@yandex.ru>
2025-07-04 08:56:35 +0300

    Array 'acBuf' added to struct 'upvs_clt_t'; func def 'upvs_clt__set_mac' added also

--- src/upvs_clt.h
+++ src/upvs_clt.h
@@ -18,21 +18,23 @@ typedef struct {
   upvs_param_t *pxPrm;
   upvs_err_t   *pxErr;
   //upvs_errdb_cell_t axErrDb[ERRDB_LENGHT];
+  u8_t acBuf[20];
 } upvs_clt_t;
 
 void *upvs_clt__create(void);
 int   upvs_clt__init(upvs_clt_t *);
 void  upvs_clt__del(upvs_clt_t *);
-void upvs_clt__set_sernum(u8_t *, ser_num_t *);
-int upvs_clt__set( upvs_clt_t *, const u8_t *, u32_t, const u8_t *, u32_t  );
-int upvs_clt__get_err( upvs_clt_t *, u32_t, u8_t *, u8_t * );
-bool upvs_clt__is_err_new(upvs_clt_t *, u32_t);
-void upvs_clt__set_err_new(upvs_clt_t *, u32_t, bool);
-bool upvs_clt__is_err_act(upvs_clt_t *, u32_t);
-void upvs_clt__set_err_act(upvs_clt_t *, u32_t, bool);
-void upvs_clt__reset_err(upvs_clt_t *, u32_t);
+void  upvs_clt__set_sernum(u8_t *, ser_num_t *);
+void  upvs_clt__set_mac(u8_t *, u8_t *, u32_t);
+int   upvs_clt__set( upvs_clt_t *, const u8_t *, u32_t, const u8_t *, u32_t  );
+int   upvs_clt__get_err( upvs_clt_t *, u32_t, u8_t *, u8_t * );
+bool  upvs_clt__is_err_new(upvs_clt_t *, u32_t);
+void  upvs_clt__set_err_new(upvs_clt_t *, u32_t, bool);
+bool  upvs_clt__is_err_act(upvs_clt_t *, u32_t);
+void  upvs_clt__set_err_act(upvs_clt_t *, u32_t, bool);
+void  upvs_clt__reset_err(upvs_clt_t *, u32_t);
 s32_t upvs_clt__get_err_code(upvs_clt_t *, u32_t);
-bool upvs_clt__get_prm_new(upvs_clt_t *, u32_t);
+bool  upvs_clt__get_prm_new(upvs_clt_t *, u32_t);
 s32_t upvs_clt__edit_prm_new(upvs_clt_t *, u32_t, bool);
 
 #endif
