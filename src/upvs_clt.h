4021ce6601b5816829db75fd5faf982057c50026
proglyk <ilyapronyashin23@yandex.ru>
2025-07-07 15:17:22 +0300

    1) Added one arg to declaration of 'upvs_clt__get_prm()'; 2) Added field 'pvCreated', 'pvDeleted' to struct 'upvs_clt_t' for callback usage

--- src/upvs_clt.h
+++ src/upvs_clt.h
@@ -6,6 +6,9 @@
 #include "upvs_prm.h"
 #include "upvs_err.h"
 
+// cb in main.c
+typedef void  (*upvs_cb_ptr_t)(void *);
+
 typedef struct {
   u32_t Digit;  // 001..999
   u32_t Month;  // 01..12
@@ -19,19 +22,26 @@ typedef struct {
   upvs_err_t   *pxErr;
   //upvs_errdb_cell_t axErrDb[ERRDB_LENGHT];
   u8_t acBuf[20];
+  upvs_cb_ptr_t pvCreated;
+  upvs_cb_ptr_t pvDeleted;
 } upvs_clt_t;
+
+
 
 // Функции создания, инициализации, удаления экземпляра упр. структуры
 void *upvs_clt__create(void);
 int   upvs_clt__init(upvs_clt_t *);
 void  upvs_clt__del(upvs_clt_t *);
+
 // Основные функции клиентской части
 int   upvs_clt__set(upvs_clt_t *, const u8_t *, u32_t, const u8_t *, u32_t);
-s32_t upvs_clt__get_prm(upvs_clt_t *, u8_t *, u8_t *, u32_t);
+s32_t upvs_clt__get_prm(upvs_clt_t *, u8_t *, u8_t *, u32_t, u32_t);
 int   upvs_clt__get_err(upvs_clt_t *, u8_t *, u8_t *, u32_t);
+
 // Вспомогательные функции
 void  upvs_clt__set_sernum(u8_t *, ser_num_t *);
 void  upvs_clt__set_mac(u8_t *, u8_t *, u32_t);
+
 // Функции-обёртки (wrappers)
 bool  upvs_clt__is_err_new(upvs_clt_t *, u32_t);
 void  upvs_clt__set_err_new(upvs_clt_t *, u32_t, bool);
