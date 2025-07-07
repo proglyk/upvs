51a7fa08a9d556d1fa6827eddc8d20bae6cabbd0
proglyk <ilyapronyashin23@yandex.ru>
2025-07-07 15:21:09 +0300

    1) 'upvs_mqtt_clt__is_conntd()' was added

--- src/upvs_mqtt_clt.h
+++ src/upvs_mqtt_clt.h
@@ -14,6 +14,7 @@ void *upvs_mqtt_clt__create(void);
 int   upvs_mqtt_clt__init(mqtt_clt_t *, s32_t);
 void  upvs_mqtt_clt__del(mqtt_clt_t *);
 s32_t	upvs_mqtt_clt__send(mqtt_clt_t *, const u8_t *, const u8_t *);
+bool  upvs_mqtt_clt__is_conntd(mqtt_clt_t *);
 // wrapper
 #define clt_inst(P) upvs_mqtt_clt__clt_inst(P)
 upvs_clt_t *upvs_mqtt_clt__clt_inst(mqtt_clt_t *self);
